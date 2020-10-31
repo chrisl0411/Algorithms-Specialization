a52779acad40e30c82eca72dcdd9a5d63fb989b6
Chris Lee <chrisl0411@gmail.com>
2020-10-31 14:46:12 -0500

    first commit

---
+++ venv/ProgrammingAssignment2.py
@@ -0,0 +1,16 @@
+#Stanford Algorithms Specialization Part 1 Divide and Conquer
+#Programming Assignment #2
+
+#Goal: compute number of inversions in the file given where the ith row of the file indicates the ith entry of an array
+
+def readFile():
+    filePath = "C:\Users\Chris\Documents\Coding\Standford Algorithms Specialization\Part 1_Divide and Conquer\_bcb5c6658381416d19b01bfc1d3993b5_IntegerArray.txt"
+    f = open(filePath, 'r')
+    
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
