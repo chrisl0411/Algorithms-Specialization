6ad335780d84f08b58c3d3c21f5875b4e4edae6f
Chris Lee <chrisl0411@gmail.com>
2020-10-31 16:39:24 -0500

    second commit

--- venv/ProgrammingAssignment2.py
+++ venv/ProgrammingAssignment2.py
@@ -2,15 +2,56 @@
 #Programming Assignment #2
 
 #Goal: compute number of inversions in the file given where the ith row of the file indicates the ith entry of an array
+#An inverstion = a pair of array indices with i<j and A[i]>A[j]
+#if x copied to merge array before y, then x<y and no inversion involving x and y
+#if y copied to merge array before x, then x>y and split inversion is present
+#when element of 2nd array C gets copied to output D, increment total by number of elements remaining in 1st array B
+
+import os
 
 def readFile():
-    filePath = "_bcb5c6658381416d19b01bfc1d3993b5_IntegerArray.txt"
-    f = open(filePath, 'r')
-    return f
+    filePath = "C:\\Users\Chris\Documents\Coding\Standford Algorithms Specialization\Part 1_Divide and Conquer\IntegerArray.txt"
+    f = open(filePath, "r")
+    array = []
+    while (f.readline()) :
+        array.append(int(f.readline()))
 
-def main():
+    length = len(array)
+    return array
 
-    print (readFile)
+def countSplitInv(x, y):
+    i = 0
+    j = 0
+    count = 0
+    result = []
+
+    while (i < len(x) and j < len(y)):
+        if (x[i] < y[j]):
+            result.append(x[i])
+            i+=1
+        else:
+            count+= len(x)-i #adds number of remaining elements in x to split inv count
+            result.append(y[j])
+            j+=1
+
+    result+=left[i:]
+    result+=right[j:]
+
+    return result, count
+
+def sort(array):
+    #high level aglorithm
+    #base case
+    n = len(array)
+    if (n == 1):
+        return 0
+    else:
+        middleIndex = int(n/2)
+        x = sort(array[:middleIndex])[0]
+        y = sort(array[middleIndex:])[0]
+        sortedlist, count = countSplitInv(x, y)
+
+    return sortedlist, count
 
 if __name__ == "__main__":
-    main()
+    print(sort(readFile())[1])
