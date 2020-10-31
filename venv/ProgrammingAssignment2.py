892f3955fdb1356e6d350ff25937ecf377e6c0f5
Chris Lee <chrisl0411@gmail.com>
2020-10-31 14:49:52 -0500

    git commit Pr

--- venv/ProgrammingAssignment2.py
+++ venv/ProgrammingAssignment2.py
@@ -4,13 +4,13 @@
 #Goal: compute number of inversions in the file given where the ith row of the file indicates the ith entry of an array
 
 def readFile():
-    filePath = "C:\Users\Chris\Documents\Coding\Standford Algorithms Specialization\Part 1_Divide and Conquer\_bcb5c6658381416d19b01bfc1d3993b5_IntegerArray.txt"
+    filePath = "_bcb5c6658381416d19b01bfc1d3993b5_IntegerArray.txt"
     f = open(filePath, 'r')
-    
+    return f
 
 def main():
 
-
+    print (readFile)
 
 if __name__ == "__main__":
     main()
